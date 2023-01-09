ec9fe05b9f109bd25b5f3d312c7f830cd8c4e8eb
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-01-09 16:44:51 +0300

    finaizing the products and product styling

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -5,23 +5,25 @@ import { MdAddShoppingCart } from "react-icons/md";
 
 
 function Product({ product }) {
-  // const ButtonsIconStyle = { color: "black" };
+  const ButtonsIconStyle = { color: "black" };
 
   return (
     <div className='product'>
       <img className="productMedia" src={product.image} alt={product.name} title={product.name} />
       <div className='productContent'>
-        <h4 className='productName'>{product.name}</h4>
-        <h4 className='productprice'>{product.price}</h4>
+        <div className='productNameAndPrice'>
+          <h4 className='productName'>{product.name}</h4>
+          <h4 className='productprice'>{product.price}</h4>
+        </div>
         <p className='productDescription'>{product.description}</p>
       </div>
       <div className='productButtonsAndIconsContainer'>
         <Tooltip title="Add to cart" arrow>
-          <btn className="btn-addToCart">
-            <IconContext.Provider value={{ className: 'react-icons-addToCart' }}>
-              <MdAddShoppingCart />
+          <button className="btn-addToCart">
+            <IconContext.Provider value={{ size: '24px' }}>
+              <MdAddShoppingCart style={ButtonsIconStyle}/>
             </IconContext.Provider>
-          </btn>
+          </button>
         </Tooltip>
       </div>
     </div>
