4d1175804938f46b60f1b43d67cd9015e9110a14
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-01-10 15:42:42 +0300

    adding hover and active events to the product images andpositioning the add-to-cart button

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -15,7 +15,8 @@ function Product({ product }) {
           <h4 className='productName'>{product.name}</h4>
           <h4 className='productprice'>{product.price.formatted_with_symbol.slice(0, product.price.formatted_with_symbol.length - 3)}</h4>
         </div>
-        <p className='productDescription'>{product.description.slice(3, product.description.length - 4)}</p>
+        <p className='productDescription' dangerouslySetInnerHTML={{ __html: product.description}}/>
+          {/* {product.description.slice(3, product.description.length - 4)}</p> */}
       </div>
       <div className='productButtonsAndIconsContainer'>
         <Tooltip title="Add to cart" arrow>
