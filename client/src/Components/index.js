9c2b40e2cb8fe61f5d0d616f990e1d192edbd00b
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-02-02 13:42:46 +0300

    styling the alert component

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,4 +3,5 @@ export {default as Product} from './Product';
 export {default as NavBar} from './NavBar';
 export {default as CustomerLogin} from './CustomerLogin';
 export {default as CustomerSignUp} from './CustomerSignUp';
-export {default as ForgotPassword} from './ForgotPassword';
+export {default as ForgotPassword} from './ForgotPassword'; 
+export {default as Alert} from './Alert';
