583894f893c0c8dc39cea13591f841dbb2e1286f
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-02-05 03:06:31 +0300

    styling bakery product component

--- client/src/Components/BakeryProducts/BakeryProduct/BakeryProduct.jsx
+++ client/src/Components/BakeryProducts/BakeryProduct/BakeryProduct.jsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconContext } from 'react-icons/lib';
-import {RxDotFilled} from 'react-icons/rx';
-// import { Tooltip } from '@mui/material';
+import { RxDotFilled } from 'react-icons/rx';
+import { SlOptions } from 'react-icons/sl';
+import { useNavigate } from 'react-router-dom';
 
 
 function BakeryProduct({ product }) {
     const activeIconStyles = { marginRight: "3px", marginLeft: "6px", color: "green" };
     const notActiveIconStyles = { marginRight: "3px", marginLeft: "6px", color: "red" };
+    const [dropdownDisplay, setDropdownDisplay] = useState("none");
+    const navigate = useNavigate();
 
     return (
         <tr className="productsDataRow">
 
             <td className="productsDataCell">
                 <div className='productImageAndNameContainer'>
-                    <img className='productImage' src={product.image} alt={product.name}/>
+                    <img className='productImage' src={product.image} alt={product.name} />
                     <p className='productName'>{product.name}</p>
                 </div>
             </td>
             <td className="productsDataCell">{product.orders}</td>
-            <td className="productsDataCell">{product.sales}</td>
+            <td className="productsDataCell">KSH {product.sales}</td>
             <td className="productsDataCell">{
-                product.active ? 
-                <div className='productActiveIconAndText'>
-                    <IconContext.Provider value={{ size: '20px' }}>
-                        <RxDotFilled style={activeIconStyles} />
-                    </IconContext.Provider>
-                    <p className='productActive'>{product.active}</p>
-                </div> 
-                : 
-                <div className='productActiveIconAndText'>
-                    <IconContext.Provider value={{ size: '20px' }}>
-                        <RxDotFilled style={notActiveIconStyles} />
-                    </IconContext.Provider>
-                    <p className='productActive'>{product.active}</p>
-                </div>
+                product.active ?
+                    <div className='productActiveIconAndText'>
+                        <IconContext.Provider value={{ size: '20px' }}>
+                            <RxDotFilled style={activeIconStyles} />
+                        </IconContext.Provider>
+                        <p className='productActive'>Active</p>
+                    </div>
+                    :
+                    <div className='productActiveIconAndText'>
+                        <IconContext.Provider value={{ size: '20px' }}>
+                            <RxDotFilled style={notActiveIconStyles} />
+                        </IconContext.Provider>
+                        <p className='productActive'>Not active</p>
+                    </div>
 
 
             }
             </td>
             <td className="productsDataCell">
-                <div>
-                    {/* <Tooltip title="options" arrow>
-
-                    </Tooltip> */}
+                <div className="dropdown">
+                    <IconContext.Provider value={{ size: '20px' }}>
+                        <SlOptions onClick={() => dropdownDisplay === "block" ? setDropdownDisplay("none") : setDropdownDisplay("block")} style={{color: "black"}} />
+                    </IconContext.Provider>
+                    <div className="dropdown-content" style={{display: dropdownDisplay}}>
+                        <p onClick={() => {
+                            //set target product here
+                            navigate("/bakery-portal/edit-product")
+                        }}>Edit</p>
+                        <p>Delete</p> 
+                    </div>
                 </div>
             </td>
         </tr>
