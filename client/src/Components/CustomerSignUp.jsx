6677f1a8adb8e4299aaa97a0e6b5496616ca21a1
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-05-06 22:22:50 +0300

    making CustomerSighUp component form inputs controlled

--- client/src/Components/CustomerSignUp.jsx
+++ client/src/Components/CustomerSignUp.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/BakeryLogo.jpeg";
+import { Alert } from "../Components";
 
 
 function CustomerSignUp() {
+    //creating form input states
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+    //creating loading state
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    function handleSignUp(){
+        const customerData = {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            verified: false, 
+            password: password, 
+            password_confirmation: passwordConfirmation
+        }
+        
+    }
+
     return (
         <div className="customerSignUpContainer">
             <div className="textContainer">
@@ -16,16 +40,16 @@ function CustomerSignUp() {
                 <form className="customerSignUpForm">
                     <h1 className="formTitle">SIGN UP</h1>
                     <label className="customerSignUpFormLabel">First Name:</label>
-                    <input className="customerSignUpFormInput" type="email" required />
+                    <input className="customerSignUpFormInput" type="text" required value={firstName} onChange={e => setFirstName(e.target.value)}/>
                     <label className="customerSignUpFormLabel">Last Name:</label>
-                    <input className="customerSignUpFormInput" type="email" required />
+                    <input className="customerSignUpFormInput" type="text" required value={lastName} onChange={e => setLastName(e.target.value)}/>
                     <label className="customerSignUpFormLabel">Email:</label>
-                    <input className="customerSignUpFormInput" type="email" required />
+                    <input className="customerSignUpFormInput" type="email" required value={email} onChange={e => setEmail(e.target.value)}/>
                     <label className="customerSignUpFormLabel">Password:</label>
-                    <input className="customerSignUpFormInput" type="password" required />
+                    <input className="customerSignUpFormInput" type="password" required  value={password} onChange={e => setPassword(e.target.value)}/>
                     <label className="customerSignUpFormLabel">Confirm Password:</label>
-                    <input className="customerSignUpFormInput" type="password" required />
-                    <button className="customerSignUpButton" type="submit">Create Account</button>
+                    <input className="customerSignUpFormInput" type="password" required value={passwordConfirmation} onChange={e => setPasswordConfirmation(e.target.value)}/>
+                    <button className="customerSignUpButton" type="submit">{isLoading ? "Loading..." : "Create Account"}</button>
                 </form>
 
             </div>
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,9 @@ Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+
+  #adding custom action routes for customers
+
+  
 end
