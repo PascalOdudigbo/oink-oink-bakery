77dda753cb74dc0ee5d65e4ba2254d1f6ec625ea
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-06-30 16:21:21 +0100

    starting render configurations for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 puts "Seeding 🌱🌱🌱" 
 
-ashley = Baker.create(first_name: "Ashley", last_name: "Simiyu", email: "jaynasimiyu@gmail.com", password: "1234", password_confirmation: "1234")
-noDiscount = Discount.create(name: "No discount", description: "The product has no discount", discount_percent: 0.0)
-noVariantGroup = VariantGroup.create(name: "No variant group")
+if Baker.all.length < 1
+    ashley = Baker.create(first_name: "Ashley", last_name: "Simiyu", email: "jaynasimiyu@gmail.com", password: "1234", password_confirmation: "1234")
+end
+
+if Discount.all.length < 1
+    noDiscount = Discount.create(name: "No discount", description: "The product has no discount", discount_percent: 0.0)
+end
+
+if VariantGroup.all.length < 1
+    noVariantGroup = VariantGroup.create(name: "No variant group")
+end
 
 puts "Done seeding!"
