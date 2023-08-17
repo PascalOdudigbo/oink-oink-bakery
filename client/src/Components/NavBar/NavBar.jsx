4a72f072aab8b5a1afcd08a631d39c40e606a44f
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-08-17 17:00:14 +0100

    working on mobile media queries for navbar and its sub-components. Also working on search component mobile media queries

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from "../../assets/BakeryLogo.jpeg";
 import Tooltip from '@mui/material/Tooltip';
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { IconContext } from "react-icons/lib";
 import { Badge } from '@mui/material';
 import { MdShoppingCart } from 'react-icons/md';
-import {LoginOrSignUpDropdown, UserSignedInDropdown} from '../../Components';
+import { LoginOrSignUpDropdown, UserSignedInDropdown } from '../../Components';
 function NavBar({ totalItems, bakerData, customerData, handleLogout }) {
     //styling the icons
     const ButtonsIconStyle = { color: "black" };
@@ -21,33 +21,38 @@ function NavBar({ totalItems, bakerData, customerData, handleLogout }) {
 
             <div className="flexibleGrow" />
 
-            {
-                customerData?.verified === true || bakerData?.first_name ? 
-                <UserSignedInDropdown customerData={customerData} bakerData={bakerData} handleLogout={handleLogout}/> : 
-                window.location.href.includes("/login") ||
-                window.location.href.includes("/admin-login") ||
-                window.location.href.includes("bakery-portal") 
-                ? null : <LoginOrSignUpDropdown/> 
-                    
-            }
+            <div className="NavBarIconsContainer">
 
-            {
-                window.location.href.includes("cart") ||
-                    window.location.href.includes("login") ||
-                    window.location.href.includes("sign-up") ||
-                    window.location.href.includes("forgot-password") ||
-                    window.location.href.includes("admin") ||
-                    // window.location.href.includes("customer") ||
-                    window.location.href.includes("bakery-portal") ? null :
-                    <Tooltip title={<p className="tooltipText">Show cart items</p>} arrow>
-                        <button className="btn-showCartItems" onClick={() => navigate("/cart")}>
-                            <IconContext.Provider value={{ size: '26px' }}>
-                                <Badge badgeContent={totalItems} color="secondary">
-                                    <MdShoppingCart style={ButtonsIconStyle} />
-                                </Badge>
-                            </IconContext.Provider>
-                        </button>
-                    </Tooltip>}
+                {
+                    customerData?.verified === true || bakerData?.first_name ?
+                        <UserSignedInDropdown customerData={customerData} bakerData={bakerData} handleLogout={handleLogout} /> :
+                        window.location.href.includes("/login") ||
+                            window.location.href.includes("/admin-login") ||
+                            window.location.href.includes("bakery-portal")
+                            ? null : <LoginOrSignUpDropdown />
+
+                }
+
+                {
+                    window.location.href.includes("cart") ||
+                        window.location.href.includes("login") ||
+                        window.location.href.includes("sign-up") ||
+                        window.location.href.includes("forgot-password") ||
+                        window.location.href.includes("admin") ||
+                        // window.location.href.includes("customer") ||
+                        window.location.href.includes("bakery-portal") ? null :
+                        <Tooltip title={<p className="tooltipText">Show cart items</p>} arrow>
+                            <button className="btn-showCartItems" onClick={() => navigate("/cart")}>
+                                <IconContext.Provider value={{ size: '26px' }}>
+                                    <Badge badgeContent={totalItems} color="secondary">
+                                        <MdShoppingCart style={ButtonsIconStyle} />
+                                    </Badge>
+                                </IconContext.Provider>
+                            </button>
+                        </Tooltip>
+                }
+
+            </div>
         </div>
     )
 }
